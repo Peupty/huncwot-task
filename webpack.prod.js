a5ad15c2087a8259186d94afb396808c88894320
mw <melvillewatford@gmail.com>
2021-09-24 04:05:16 +0200

    add build config

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  mode: "production",
+  entry: "./src/scripts/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      { test: /\.(html)$/, use: ["html-loader"] },
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    port: 8000,
+    hot: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true
+    }),
+  ],
+}
