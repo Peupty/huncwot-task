a3ae215ddc537a6bde9e697e0e0ad3d2b4d61a05
mw <melvillewatford@gmail.com>
2021-09-24 03:39:47 +0200

    add main script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-alert("hello world");
+import "../styles/scss/main.scss"
+import setupSlider from "./setupSlider"
+import scrollAnimations from "./scrollAnimations"
+;(async () => {
+  "useStrict"
+  window.onload = document.querySelector("body").style.display = "initial"
+  await setupSlider()
+  await scrollAnimations()
+})()
